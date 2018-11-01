5391409d89754fe06553b5ce55012ccddb1867cf
PS <github.ps@gmail.com>
2018-11-01 19:24:41 +0100

    added posix compliant output rerouting

--- run_results.sh
+++ run_results.sh
@@ -21,7 +21,9 @@ IS_BINARY="$2"
 export PYTHONPATH="./perfect_match/:$PYTHONPATH"
 # The following command merges all run files for repeated runs (run_$i/run.txt)
 # into a single summary file ($FOLDER_PATH/summary.txt).
-python ./perfect_match/apps/main.py --dataset=./ --with_rnaseq --do_train --do_hyperopt --num_hyperopt_runs=10 --do_evaluate --fraction_of_data_set=1.00 --num_units=16 --num_layers=2 --seed=909 --num_epochs=100 --learning_rate=0.001 --dropout=0.0 --batch_size=4 --do_merge_lsf --l2_weight=0.000 --imbalance_loss_weight=0.0 --benchmark=jobs --method=nn --early_stopping_patience=7 --do_not_save_predictions --validation_set_fraction=0.24 --test_set_fraction=0.2 --with_propensity_batch --early_stopping_on_pehe --experiment_index=27 --num_treatments=2 --output_directory=$FOLDER_PATH &> /dev/null
+python ./perfect_match/apps/main.py --dataset=./ --with_rnaseq --do_train --do_hyperopt --num_hyperopt_runs=10 --do_evaluate --fraction_of_data_set=1.00 --num_units=16 --num_layers=2 --seed=909 --num_epochs=100 --learning_rate=0.001 --dropout=0.0 --batch_size=4 --do_merge_lsf --l2_weight=0.000 --imbalance_loss_weight=0.0 --benchmark=jobs --method=nn --early_stopping_patience=7 --do_not_save_predictions --validation_set_fraction=0.24 --test_set_fraction=0.2 --with_propensity_batch --early_stopping_on_pehe --experiment_index=27 --num_treatments=2 --output_directory=$FOLDER_PATH > /dev/null 2>&1
+
+wait
 
 count=$(cat $FOLDER_PATH/summary.txt | grep Best_test_score  | wc -l)
 
